6ede2d42ccce779b7597c4a72932bfb443095246
sky <sky.ilyashenko@gmail.com>
2016-08-17 17:56:36 +0300

    version 1

--- app/server.js
+++ app/server.js
@@ -1,12 +1,56 @@
 var http = require('http'),
     static = require('node-static'),
-    indexFile = new static.Server('../');
+    url = require('url'),
+    querystring = require('querystring'),
+    indexFile = new static.Server('../'),
+    subscribers = {};
 
 http.createServer(accept).listen(8080);
 console.log('created dialogMe server');
 
 function accept(request, response) {
-    console.log(request.url);
+    var parsedUrl = url.parse(request.url),
+        pathname = parsedUrl.pathname,
+        query = parsedUrl.query,
+        id = querystring.parse(query).id,
+        message;
+
+    console.log(pathname + ' ' + id);
+
+    if (pathname === '/publish') {
+        message = '';
+        request.on('data', function(chunk) {
+            message += chunk;
+        }).on('end', function () {
+            publish(id + ": " + message);
+        });
+        response.end('ok');
+        return;
+    }
+
+    if (pathname === '/subscribe') {
+        onSubscribe(request, response, id);
+        return;
+    }
 
     indexFile.serve(request, response);
+}
+
+function publish(message) {
+    var id;
+    console.log(message);
+
+    for (id in subscribers) {
+        subscribers[id].end(message);
+    }
+    subscribers = {};
+}
+
+function onSubscribe(request, response, id) {
+    subscribers[id] = response;
+    console.log(Object.keys(subscribers));
+
+    request.on('close', function () {
+        delete subscribers[id];
+    });
 }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log('hello main.js');
+function formSend(form, url) {
+    form.onsubmit = function () {
+        var message = form.message.value;
+
+        if (message) {
+            publish(message);
+            form.message.value = '';
+        }
+        return false;
+    };
+
+    function publish(message) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.send(message);
+    }
+}
+
+function subscribePane(pane, url) {
+    subscribe();
+
+    function subscribe() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) {
+                return;
+            }
+
+            if (this.status === 200) {
+                addMessage(this.responseText);
+                subscribe();
+                return;
+            }
+
+            if (this.status === 404) {
+                addMessage(this.statusText);
+            }
+
+            setTimeout(subscribe, 1500);
+        };
+        xhr.send();
+    }
+
+    function addMessage(message) {
+        var messageElement = document.createElement('div');
+        messageElement.appendChild(document.createTextNode(message));
+        pane.appendChild(messageElement);
+    }
+}
